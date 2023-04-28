e5e690e7dc5c2d51a21d95462207a8022ce1401c
Dmitry Uvarov <diman_niks@mail.ru>
2023-04-28 12:35:14 +0300

    feat: solution 1 task

--- 1 task/index.js
+++ 1 task/index.js
@@ -5,14 +5,58 @@ const timerEl = document.querySelector('span');
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  let timer = null;
+
+  return (seconds) => {
+    if (timer) clearInterval(timer);
+    
+    const start = Date.now();
+    let prevDelta = 0;
+
+    timerEl.textContent = getTime(seconds);
+
+    timer = setInterval(() => {
+      const delta = Math.floor((Date.now() - start) / 1000);
+      const difference = delta - prevDelta;
+
+      if (difference > 0) {
+        seconds = seconds + difference;
+        prevDelta = delta;
+ 
+        timerEl.textContent = getTime(seconds);
+      }
+    }, 10);
+  };
 };
+
+const getTime = (seconds) => {
+  const time = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+  
+  if (seconds > (60 * 60)) time.hours = Math.floor(seconds / 60 / 60);
+  if (seconds > 60) time.minutes = Math.floor(seconds / 60) % 100;
+  time.seconds = seconds % 60;
+
+  return Object.values(time).map((num) => num < 10 ? `0${num}` : `${num}`).join(':');
+};
+
 
 const animateTimer = createTimerAnimator();
 
 inputEl.addEventListener('input', () => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+  const target = e.target;
+  const { value } = target;
+
+  if (!value.length > 0) return;
+
+  if (!/^\d+$/.test(value)) {
+    target.value = value.replace(/\D/g, '');
+  } 
 });
 
 buttonEl.addEventListener('click', () => {
